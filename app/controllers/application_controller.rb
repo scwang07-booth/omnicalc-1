5071f2eaa0a1ab3b6ceafa9e6a31383d06db0ffe
scwang07 <swang20@chicagobooth.edu>
2023-01-21 18:00:20 +0000

    blank square form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-
+  def blank_square_form
+    render({:template => "calc_templates/square_form.html.erb"})
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  get("/square/new", { :controller => "application", :action => "blank_square_form"})
+  get("/square_root/new", { :controller => "application", :action => "blank_sqroot_form"})
+  get("/payment/new", { :controller => "application", :action => "blank_payment_form"})
+  get("/random/new", { :controller => "application", :action => "blank_random_form"})
 end
