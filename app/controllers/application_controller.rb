6bc2e8e6d289603170fe0c57db64a61c029c589e
scwang07 <swang20@chicagobooth.edu>
2023-01-21 18:15:28 +0000

    Calculate square complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   def blank_square_form
     render({:template => "calc_templates/square_form.html.erb"})
-    
   end
+  def calculated_square
+    @num = params.fetch("number").to_i
+    @square_of_num = @num * @num
+
+    render({:template => "calc_templates/square_results.html.erb"})
+  end
+
 end
